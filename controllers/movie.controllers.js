c222773e9adaeca9cbb3302dfca373ad8ebb598f
Harishbabu2005 <harishbabups2005@gmail.com>
2025-06-18 14:03:13 +0530

    add model

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -1,9 +1,21 @@
+import Movie from "../models/movie.models.js";
+
 export const movieview=(req,res)=>{
     res.send("hi")
 }
-export const moviecreate=(req,res)=>{
-   console.log(req.body)
-   return res.json(req.body);
+export const moviecreate=async(req,res)=>{
+  
+    const newMovie=new Movie({
+        title : req.body.title,
+        desc : req.body.desc
+    })
+    try {
+        const movie=await newMovie.save();
+        return res.status(201).json(movie)
+    } catch (error) {
+        return res.status(400).json({message:error.message});
+    }
+  
     
 }
 export const movieupdate=(req,res)=>{
